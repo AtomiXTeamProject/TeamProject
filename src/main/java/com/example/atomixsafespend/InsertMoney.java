1420983bfbc38bb8cf2534b9adc7b46aaba74986
ishaanR87 <55754104+ishaanR87@users.noreply.github.com>
2021-03-15 13:44:03 +0000

    Add files via upload
    First two pages for insert money with button switching pages

--- src/main/java/com/example/atomixsafespend/InsertMoney.java
+++ src/main/java/com/example/atomixsafespend/InsertMoney.java
@@ -9,6 +9,6 @@ public class InsertMoney extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_insert_money);
     }
 }
--- src/main/java/com/example/atomixsafespend/MainActivity.java
+++ src/main/java/com/example/atomixsafespend/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.atomixsafespend;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button insertMoney = (Button) findViewById(R.id.insertbutton);
+        insertMoney.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                Intent switchscreens = new Intent(MainActivity.this, InsertMoney.class);
+                startActivity(switchscreens);
+            }
+        });
+      }
     }
-}
